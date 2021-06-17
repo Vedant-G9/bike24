a8d192a15c1b622e4d46962970ae8c13d6b40e38
pyvedantgiri <vedantgosai007@gmail.com>
2021-06-17 12:54:22 +0530

    FINAL

--- src/components/Layout/Seaction.js
+++ src/components/Layout/Seaction.js
@@ -17,7 +17,7 @@ const Seaction = () => {
         </div>
         <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
           <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
-            Bike two four
+            Bike 24x
           </h1>
           <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
             You are on your own. You are not protected by two tons of steel,
